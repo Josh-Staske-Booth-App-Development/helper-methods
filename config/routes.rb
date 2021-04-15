b9975c83f607a326f83157fec62e8ccd24290ccb
staskebooth <jstaske@chicagobooth.edu>
2021-04-15 21:23:02 +0000

    Finished updating routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   # Routes for the Movie resource:
 
   # CREATE
-  post"/movies" => "movies#create" as: :movies
-  get"/movies/new" => "movies#new" as: :new_movie
+  post"/movies" => "movies#create", as: :movies
+  get"/movies/new" => "movies#new", as: :new_movie
           
   # READ
   get"/movies" => "movies#index"
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   
   # UPDATE
   patch"/movies/:id" => "movies#update"
-  get"/movies/:id/edit" => "movies#edit" as: :edit_movie
+  get"/movies/:id/edit" => "movies#edit", as: :edit_movie
   
   # DELETE
   delete"/movies/:id" => "movies#destroy"
